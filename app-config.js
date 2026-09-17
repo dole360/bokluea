0c68f80ac92d50300d4ae74c6ac58e85a165cf7b
dole360 <muaisds@gmail.com>
2026-09-17 16:03:10 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 // LP360 District central configuration
 // เปลี่ยน Apps Script /exec ของเว็บไซต์อำเภอที่ไฟล์นี้เพียงจุดเดียว
 window.APP_CONFIG = Object.freeze({
-  EXEC_URL: 'https://script.google.com/macros/s/AKfycbyGRs8U6Y_90v4vp-b89DbPys6XdK10wNfk6wr9GlOodS56eCmt9mRAQaor06sPXSyw/exec',
+  EXEC_URL: 'https://script.google.com/macros/s/AKfycbwPHNhM5pAA6hu4laMzxQT3bg_6lh3ob-l_NB7HaH0cZQ_kbxOyXUVwCjjXRZyR87ke/exec',
   SITE_TYPE: 'DISTRICT',
   CACHE_PREFIX: 'LP360:DISTRICT:'
 });
